9b85500fc541987a99a6612bbfa64163dfeb4ded
waskylinks <likitajoel@gmail.com>
2025-11-04 13:17:44 +0100

    feat: port

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`)
+})
+
+
 
