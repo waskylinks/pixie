dc5360888c5f0ce4f76d55bcaf8b47c097295c1c
waskylinks <likitajoel@gmail.com>
2025-11-06 11:50:56 +0100

    feat: login controller using jsonwebtoken logic

--- Controllers/auth-controller.js
+++ Controllers/auth-controller.js
@@ -2,6 +2,8 @@ const User = require('../Models/User');
 
 const bcrypt = require('bcryptjs');
 
+const jwt = require('jsonwebtoken');
+
 // register controller
 const registerUser = async(req, res) => {
     try{
@@ -63,6 +65,43 @@ const registerUser = async(req, res) => {
 //login controller
 const loginUser = async(req, res) => {
     try{
+        const {username, password} = req.body;
+
+        //check if user exists
+        const user = await User.findOne({
+            username
+        });
+        if(!user) {
+            return res.status(400).json({
+                success: false,
+                message: "User doesn't exists"
+            });
+        }
+
+        //if the password is correct or not
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+        if(!isPasswordMatch) {
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid credentials'
+            });
+        }
+
+        //create user token
+        const accessToken = jwt.sign({
+            userId : user._id,
+            username : user.username,
+            role : user.role,
+        }, process.env.JWT_SECRET_KEY, {
+            expiresIn : '15m'
+        });
+
+        res.status(200).json({
+            success : true,
+            message: 'Logged in successfully',
+            accessToken
+        })
+
 
     } catch(e) {
         console.log(e);
