1c5ada56278726a25c67b9a2460186b81ad6504d
waskylinks <likitajoel@gmail.com>
2025-11-07 12:15:55 +0100

    feat: login authentication, send userinfo to frontend

--- Middleware/auth-middleware.js
+++ Middleware/auth-middleware.js
@@ -1,6 +1,35 @@
+//to decode token
+const jwt = require('jsonwebtoken');
+
+
 const authMiddleware = (req, res, next) => {
-    console.log('auth middleware is called')
-    next()
+    const authHeader = req.headers['authorization'];
+    console.log(authHeader);
+
+    const token = authHeader && authHeader.split(' ')[1];
+    if(!token) {
+        return res.status(401).json({
+            success: false,
+            message: 'Access denied no token provided, please log in to continue'
+        });
+    }
+
+    //decode the token and verify
+    try{
+        const decodedTokenInfo = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        console.log(decodedTokenInfo);
+
+        req.user = decodedTokenInfo;
+
+        next();
+
+    } catch(e) {
+        return res.status(500).json({
+            success: false,
+            message: 'Access denied no token provided, please log in to continue'
+        });
+    }
+
 }
 
 module.exports = authMiddleware;
--- Routes/home-routes.js
+++ Routes/home-routes.js
@@ -4,8 +4,17 @@ const authMiddleware = require('../Middleware/auth-middleware');
 
 
 router.get('/welcome', authMiddleware, (req, res) => {
+
+    //pass user info to frontend
+    const {username, userId, role} = req.userInfo;
+
     res.json({
-        message: 'Welcome to the home page'
+        message: 'Welcome to the home page',
+        user: {
+            _id: userId,
+            username,
+            role,
+        }
     });
 });
 
